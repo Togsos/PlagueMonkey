7d015f654d063493be3a6cc89c6be494d8de1b51
Togsos <115413036+Togsos@users.noreply.github.com>
2022-11-13 12:22:09 +0000

    movementStartcommit3withComments

--- PlagueMonkey.cpp
+++ PlagueMonkey.cpp
@@ -4,33 +4,36 @@
 int main()
 
 {
-   const int windowWidth {1200};
-   const int windowHeight {750};
-   char WindowName[] = "Plague Doctor";
+   const int windowWidth {1200}; //defines the window width
+   const int windowHeight {750};  //defines the window heights
+   char WindowName[] = "Plague Doctor"; //names the pop up winddow "Plague Doctor"
 
-   const int jumpHeight{100};
-    const int MOVING_UP{-1};
+   const int jumpHeight{150};//sets the maximum jump height
+    const int MOVING_UP{-1}; //control the jump speed
     const int CHANGE_DIRECTION{-1};
-    const int JUMP_INCREMENT{10};
+    const int JUMP_INCREMENT{10}; //control the jump increment
 
-    bool isJumping{false};
+    bool isJumping{false}; 
     int jumpDirection{MOVING_UP}, plagueDoctorFacing{1};
 
    
 InitWindow(windowWidth, windowHeight, WindowName);
-  const char msg[] = "Merrie Olde England 1349...";
-   Font font = LoadFont("resources/fonts/alagard.png");   
-   Vector2 fontPosition = { windowWidth/4.0f - MeasureTextEx
-   (font, msg, (float)font.baseSize*2.0f, 10).x/10,windowHeight/4.0f - font.baseSize};
 
+   //onscreen text
+  const char msg[] = "Merrie Olde England 1349..."; // unchanging message  
+   Font font = LoadFont("resources/fonts/alagard.png");  // loading the alagard.png font 
+   Vector2 fontPosition = { windowWidth/3.0f - MeasureTextEx
+   (font, msg, (float)font.baseSize*2.0f, 10).x/10,windowHeight/4.0f - font.baseSize}; //positioning the font based of halves and quarters of the width of the screen
 
+   //sprite
    int frame{0};
-    Texture2D plagueDoctor = LoadTexture("resources/textures/plagueDoctor.png");
-    Rectangle plagueDoctorRec = {float((plagueDoctor.width / 6) * frame), 0, 128, 128};
+    Texture2D plagueDoctor = LoadTexture("resources/textures/plagueDoctor.png"); //loading in the sprite
+    Rectangle plagueDoctorRec = {float((plagueDoctor.width /6) * frame), 0, 128, 128}; //there are 6 conditions of the sprite to be used
     const float TOP_OF_JUMP{windowHeight - plagueDoctorRec.height - jumpHeight};
     const float BOTTOM_OF_JUMP{windowHeight - plagueDoctorRec.height};
-    Vector2 plagueDoctorPos = {windowWidth / 2 - plagueDoctorRec.width / 2, BOTTOM_OF_JUMP};
+    Vector2 plagueDoctorPos = {windowWidth / 6 - plagueDoctorRec.width / 6, BOTTOM_OF_JUMP}; //initial postioning of the sprite
 
+   //background
    Texture2D background = LoadTexture("resources/textures/england_background.png");
     Texture2D midground = LoadTexture("resources/textures/england_midground.png");
     Texture2D foreground = LoadTexture("resources/textures/england_foreground.png");
@@ -39,15 +42,15 @@ InitWindow(windowWidth, windowHeight, WindowName);
     float scrollingMid = 0.0f;
     float scrollingFore = 0.0f;
 
-SetTargetFPS(60);
+SetTargetFPS(60); //refresh rate
 
 
 
-while(!WindowShouldClose())
+while(!WindowShouldClose()) //as long as the window is not closed do the actions contained within curly bracers
 
 
 {
-
+//scrolling speeds
  scrollingBack -= 0.1f;
         scrollingMid -= 0.5f;
         scrollingFore -= 1.0f;
@@ -81,15 +84,15 @@ DrawTextEx(font, msg, fontPosition, (float)font.baseSize*2.0f, 10, RED);
             isJumping = true;
         }
 
-        if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))
+        if (IsKeyDown(KEY_LEFT))
         {
-            plagueDoctorFacing = -1;
-            plagueDoctorPos.x -= 10;
+            plagueDoctorFacing = -1; // facing left
+            plagueDoctorPos.x -= 10; //use a compond assignment for pos.x
         }
-        else if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D))
+        else if (IsKeyDown(KEY_RIGHT))
         {
-            plagueDoctorFacing = 1;
-            plagueDoctorPos.x += 10;
+            plagueDoctorFacing = 1; //facing right
+            plagueDoctorPos.x += 10;//use a compond assignment for pos.x
         }
         plagueDoctorRec.width = 128 * plagueDoctorFacing;
 
@@ -117,8 +120,9 @@ EndDrawing();
 } 
 UnloadFont(font);
 UnloadTexture(plagueDoctor);
+
 CloseWindow();
-return 0;
+
 }
 
 
